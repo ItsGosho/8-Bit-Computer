9d8894ee200dcffe613d8031c27327072bcfd0d6
itsgosho <itsgosho2@gmail.com>
2022-07-28 01:42:47 +0300

    Shifting logic

--- EEPROM_Programmer/src/main.cpp
+++ EEPROM_Programmer/src/main.cpp
@@ -10,25 +10,24 @@
 #define SHIFT_REGISTER_D6_PIN 8
 #define SHIFT_REGISTER_D7_PIN 9
 
-#define SHIFT_REGISTER_SER_PIN 10
-#define SHIFT_REGISTER_OE_PIN 11
-#define SHIFT_REGISTER_RCLK_PIN 12
-#define SHIFT_REGISTER_SR_CLK_PIN 13
+#define SHIFT_REGISTER_SER_PIN 2
+#define SHIFT_REGISTER_RCLK_PIN 3
+#define SHIFT_REGISTER_SR_CLK_PIN 4
 
 void setup() {
     Serial.begin(9600);
     Serial.println("EEPROM Start!");
-    pinMode(SHIFT_REGISTER_D0_PIN, INPUT);
-    pinMode(SHIFT_REGISTER_D1_PIN, INPUT);
-    pinMode(SHIFT_REGISTER_D2_PIN, INPUT);
-    pinMode(SHIFT_REGISTER_D3_PIN, INPUT);
-    pinMode(SHIFT_REGISTER_D4_PIN, INPUT);
-    pinMode(SHIFT_REGISTER_D5_PIN, INPUT);
-    pinMode(SHIFT_REGISTER_D6_PIN, INPUT);
-    pinMode(SHIFT_REGISTER_D7_PIN, INPUT);
+    //pinMode(SHIFT_REGISTER_D0_PIN, INPUT);
+    //pinMode(SHIFT_REGISTER_D1_PIN, INPUT);
+    //pinMode(SHIFT_REGISTER_D2_PIN, INPUT);
+    //pinMode(SHIFT_REGISTER_D3_PIN, INPUT);
+    //pinMode(SHIFT_REGISTER_D4_PIN, INPUT);
+    //pinMode(SHIFT_REGISTER_D5_PIN, INPUT);
+    //pinMode(SHIFT_REGISTER_D6_PIN, INPUT);
+    //pinMode(SHIFT_REGISTER_D7_PIN, INPUT);
 
     pinMode(SHIFT_REGISTER_SER_PIN, OUTPUT);
-    pinMode(SHIFT_REGISTER_OE_PIN, OUTPUT);
+    //pinMode(SHIFT_REGISTER_OE_PIN, OUTPUT);
     pinMode(SHIFT_REGISTER_RCLK_PIN, OUTPUT);
     pinMode(SHIFT_REGISTER_SR_CLK_PIN, OUTPUT);
 }
@@ -60,11 +59,11 @@ void writeBitToShiftRegister(const bool& bit) {
 
     delayMicroseconds(1);
 
-  /*  digitalWrite(SHIFT_REGISTER_RCLK_PIN, HIGH);
-    delayMicroseconds(1);
-    digitalWrite(SHIFT_REGISTER_RCLK_PIN, LOW);
+    /*  digitalWrite(SHIFT_REGISTER_RCLK_PIN, HIGH);
+      delayMicroseconds(1);
+      digitalWrite(SHIFT_REGISTER_RCLK_PIN, LOW);
 
-    delayMicroseconds(1);*/
+      delayMicroseconds(1);*/
 }
 
 void clearShiftRegister() {
@@ -94,20 +93,38 @@ void test1() {
     delay(1000);
 }
 
+void test(const int& address, const bool& outputEnable) {
+    int shiftOutData = (outputEnable << 11) | address;
+
+    int msb = shiftOutData >> 8;
+    int lsb = shiftOutData & 0b0000000011111111;
+
+    shiftOut(SHIFT_REGISTER_SER_PIN, SHIFT_REGISTER_SR_CLK_PIN, MSBFIRST, msb);
+    shiftOut(SHIFT_REGISTER_SER_PIN, SHIFT_REGISTER_SR_CLK_PIN, MSBFIRST, lsb);
+
+    digitalWrite(SHIFT_REGISTER_RCLK_PIN, HIGH);
+    digitalWrite(SHIFT_REGISTER_RCLK_PIN, LOW);
+}
+
 void loop() {
 
     //test1();
 
-    clearShiftRegister();
-    shiftOut(SHIFT_REGISTER_SER_PIN, SHIFT_REGISTER_SR_CLK_PIN, MSBFIRST, 0b11010110);
-
+    //clearShiftRegister();
+/*    shiftOut(SHIFT_REGISTER_SER_PIN, SHIFT_REGISTER_SR_CLK_PIN, MSBFIRST, 0b111);
+    shiftOut(SHIFT_REGISTER_SER_PIN, SHIFT_REGISTER_SR_CLK_PIN, MSBFIRST, 0b11111111);
+    //shiftOut(SHIFT_REGISTER_SER_PIN, SHIFT_REGISTER_SR_CLK_PIN, MSBFIRST, 0b11010110);
     digitalWrite(SHIFT_REGISTER_RCLK_PIN, HIGH);
-    delayMicroseconds(1);
-    digitalWrite(SHIFT_REGISTER_RCLK_PIN, LOW);
-    delayMicroseconds(1);
+    digitalWrite(SHIFT_REGISTER_RCLK_PIN, LOW);*/
 
-    printRegisterData();
-    delay(1000);
+    for (int i = 1; i <= 2047; ++i) {
+        test(i, true);
+        delay(200);
+    }
+
+
+    //printRegisterData();
+    delay(1000000);
 
     //shiftOut(SHIFT_REGISTER_SER_PIN,)
 
